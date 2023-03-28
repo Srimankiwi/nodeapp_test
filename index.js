fb05a01475e14ca87f2d69cb7ad24e8dc5560291
root <root@ip-172-31-36-253.ap-south-1.compute.internal>
2023-03-28 10:27:16 +0000

    1st add

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello From C8" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "C8 FUTURE IS HERE" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": "C8 APP Great!, It works!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
